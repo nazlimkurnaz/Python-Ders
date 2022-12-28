9d70d88711ff45c22648454130c32f6625600af8
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2022-12-28 22:31:17 +0300

    new file:   hangisayibuyuk.py

---
+++ hangisayibuyuk.py
@@ -0,0 +1,13 @@
+while(True):
+    x = int (input ('1.sayiyi Giriniz'))
+    y = int (input ('2.sayiyi Giriniz'))
+
+    if x>y  :
+    print ('1. sayi 2. sayidan buyuktur' )
+
+    elif:
+    print ('2. sayi 1. sayidan buyuktur' )
+
+    else:
+    print ('1. sayi 2. sayiya esittir!')
+    
