45a26ba801992085c9db5d7e328285d80f5a26d9
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2022-12-28 22:34:32 +0300

    modified:   hangisayibuyuk.py

--- hangisayibuyuk.py
+++ hangisayibuyuk.py
@@ -2,12 +2,11 @@ while(True):
     x = int (input ('1.sayiyi Giriniz'))
     y = int (input ('2.sayiyi Giriniz'))
 
-    if x>y  :
-    print ('1. sayi 2. sayidan buyuktur' )
+    if (x>y):
+        print ('1. sayi 2. sayidan buyuktur' )
 
-    elif:
-    print ('2. sayi 1. sayidan buyuktur' )
+    elif (y<x):
+        print ('2. sayi 1. sayidan buyuktur' )
 
     else:
-    print ('1. sayi 2. sayiya esittir!')
-    
+        print ('1. sayi 2. sayiya esittir!')
