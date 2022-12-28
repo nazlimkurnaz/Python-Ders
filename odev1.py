8461e05b23d2c1313f360315f39c45f8b35c81b4
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2022-12-28 20:59:20 +0300

    first commit

---
+++ odev1.py
@@ -0,0 +1,19 @@
+x = int (input('ilksayı'))
+
+y= int (input('ikincisayi'))
+
+toplam= x + y
+print(toplam)
+
+çıkartma = x-y
+print(çıkartma)
+
+çarpma = x*y    
+print(çarpma)
+
+bolme = x/y
+print(bolme)
+
+
+
+
---
+++ test.py
@@ -0,0 +1,42 @@
+print('hello world')
+print('hello world')
+print('w'*3 +'.' + 'google')
+print(25*10)
+print('5**3')
+print('19'+'98')
+X = 'naz'
+print (X)
+X = 'naz'
+Y = 'kur'
+print (len(X) + len(Y))
+
+pi=3.14
+yaricap=5
+alan=pi*(yaricap**2)
+print(alan)
+
+ocak = subat = mayis = temmuz = agustos = ekim = aralik = 31
+nisan = haziran = eylul = kasim = 30
+subat = 28
+print (aralik)
+
+
+elektrikKullanim = 300
+elektrikFatura= 100
+birimFiyat = elektrikFatura / elektrikKullanim
+print(birimFiyat)
+
+gunlukKullanim = elektrikKullanim / subat
+print (gunlukKullanim)
+
+dogukan = 'memur'
+kurnaz = 'ogrenci'
+
+x = str (input('Adiniz Soyadiniz: '))
+print('Hosgeldiniz!' + ' ' + x)
+
+
+
+
+
+
